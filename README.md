288df67105a9c051620effe35ec0dbabb0646c64
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-03-01 12:59:08 -0800

    initial check-in.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Search Filter for the UCSF Library website
+
+This plugin is a glorified kludge that solves an interoperability issue between the Relevanssi and Elementor plugins.
---
+++ ucsflibrary-search-filter.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: UCSF Library Search Filter
+ * Plugin URI: https://github.com/ucsf-ckm/ucsflibrary-search-filter
+ * Description: Fixes interoperability issues between the Elementor and Relevanssi plugins.
+ * Author: Stefan Topfstedt
+ * Version: 1.0.0
+ * Author URI: https://github.com/stopfstedt
+ * License: MIT
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+add_filter('relevanssi_excerpt_content', 'ucsflibrary_search_filter_excerpt_content');
+
+function ucsflibrary_search_filter_excerpt_content($content, $post = null, $query = null) {
+    if (false === strpos($content, '<pre><code>')) {
+        return $content;
+    }
+
+    $filteredChunks = array();
+
+    $chunks = explode('<pre><code>', $content);
+    for ($i = 0; $i < count($chunks); $i++) {
+        $chunk = $chunks[$i];
+        if (false !== strpos($chunk, '</code></pre>')) {
+            $subChunks = explode('</code></pre>', $chunk);
+            $filteredChunks[] = html_entity_decode($subChunks[0]);
+            $filteredChunks[] = $subChunks[1];
+        } else {
+            $filteredChunks[] = $chunk;
+        }
+    }
+
+    return implode(' ', $filteredChunks);
+}
